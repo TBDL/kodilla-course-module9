cf929e1e4b3bdf98e737d5c826e341cc0aecf8ee
Tomasz Badelek <tbadelek@gmail.com>
2017-08-30 22:44:11 +0200

    9.1

---
+++ src/main/java/com/kodilla/good/patterns/challenges/MovieStore.java
@@ -0,0 +1,39 @@
+package com.kodilla.good.patterns.challenges;
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class MovieStore {
+
+    public Map<String, List<String>> getMovies() {
+
+        List<String> ironManTranslations = new ArrayList<>();
+        ironManTranslations.add("Żelazny człowiek");
+        ironManTranslations.add("Iron man");
+
+        List<String> avengersTranslations = new ArrayList<>();
+        avengersTranslations.add("Mściciele");
+        avengersTranslations.add("Avengers");
+
+        List<String> flashTranslations = new ArrayList<>();
+        flashTranslations.add("Błyskawica");
+        flashTranslations.add("Flash");
+
+        Map<String, List<String>> booksTtlesWithTranslations = new HashMap<>();
+        booksTtlesWithTranslations.put("IM", ironManTranslations);
+        booksTtlesWithTranslations.put("AV", avengersTranslations);
+        booksTtlesWithTranslations.put("FL", flashTranslations);
+
+
+        return booksTtlesWithTranslations;
+    }
+
+    public static void main (String [] args) {
+
+        MovieStore movieStore = new MovieStore();
+        String titles = movieStore.getMovies().entrySet().stream()
+                .flatMap(a -> a.getValue().stream())
+                .collect(Collectors.joining(", "));
+        System.out.println(titles);
+
+    }
+}
